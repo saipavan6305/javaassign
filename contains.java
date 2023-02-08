6e2f5b0508ce16c5b8666725da7b52cefcf15c3d
Naresh143H <nareshyarraa143@gmail.com>
2023-02-08 07:50:19 +0530

    update
    intial commit

---
+++ contains.java
@@ -0,0 +1,9 @@
+public class contains {
+    public static void main(String[] args) {
+        String str = "Hello Javatpoint readers";
+        boolean isContains = str.contains("sai");
+        System.out.println(isContains);
+        // Case Sensitive
+        System.out.println(str.contains("sai"));
+    }
+}
